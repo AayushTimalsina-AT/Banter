9ab53d75728853e720ef91781c2fab22386b540d
AyushTimalsina <ayushtimalsina19@gmail.com>
2023-11-07 10:31:39 +0545

    ask permissiom code changes

---
+++ app/src/main/java/com/banter/PermissionRequestActivity.java
@@ -0,0 +1,14 @@
+package com.banter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PermissionRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_request);
+    }
+}
---
+++ app/src/main/java/com/banter/Utils/MessageEvent.java
@@ -0,0 +1,2 @@
+package com.banter.Utils;public class MessageEvent {
+}
